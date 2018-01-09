0ea7b1b99a4f2f74a6fe7c3f71a5b8cb7efc5e74
DarushDev <darushfaqiri@gmail.com>
2018-01-09 12:27:22 +0800

    DataOutputStream example

---
+++ DataOutputStreamDemo.java
@@ -0,0 +1,21 @@
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+/**
+ * Created by Darush on 1/9/2018.
+ */
+public class DataOutputStreamDemo {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fout = new FileOutputStream("files/doutTest.txt");
+            DataOutputStream dout = new DataOutputStream(fout);
+            dout.writeInt(65);
+            dout.flush();
+            dout.close();
+            fout.close();
+            System.out.println("Success...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
