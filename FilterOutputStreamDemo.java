f5abcd4dcff91b9967dca4dfba773d6e1f0c24ed
DarushDev <darushfaqiri@gmail.com>
2018-01-10 14:04:21 +0800

    FilterOutputStream example

---
+++ FilterOutputStreamDemo.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
+
+/**
+ * Created by Darush on 1/10/2018.
+ */
+public class FilterOutputStreamDemo {
+    public static void main(String[] args) {
+        try {
+            File file = new File("files/test.txt");
+            FileOutputStream fileOut = new FileOutputStream(file);
+            FilterOutputStream filterOut = new FilterOutputStream(fileOut);
+            String s = "Hello World!";
+            byte[] bytes = s.getBytes();
+            filterOut.write(bytes);
+            filterOut.flush();
+            filterOut.close();
+            fileOut.close();
+            System.out.println("Success...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
