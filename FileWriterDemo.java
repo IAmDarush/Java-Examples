e43b2082c76ee807c5078ff20ab89df4a0d5ad97
DarushDev <darushfaqiri@gmail.com>
2018-01-16 15:59:42 +0800

    FileWriter example

---
+++ FileWriterDemo.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+
+/**
+ * Created by Darush on 1/16/2018.
+ */
+public class FileWriterDemo {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("files/test.txt");
+            String string = "File Writer text";
+            writer.write(string);
+            writer.close();
+            System.out.println("Done...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
