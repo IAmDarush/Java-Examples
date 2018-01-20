1a05a2b6d70e733e8048e4f90085f4b043875847
DarushDev <darushfaqiri@gmail.com>
2018-01-20 16:40:14 +0800

    StringReader example

---
+++ StringReaderDemo.java
@@ -0,0 +1,19 @@
+import java.io.StringReader;
+
+/**
+ * Created by Darush on 1/20/2018.
+ */
+public class StringReaderDemo {
+    public static void main(String[] args) {
+        try {
+            String str = "Hello String Reader Demo!!";
+            StringReader reader = new StringReader(str);
+            int i;
+            while((i = reader.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
