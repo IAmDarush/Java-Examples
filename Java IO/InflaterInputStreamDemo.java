1a75956d53acb9eca804ca9bcd34d88b28c85398
DarushDev <darushfaqiri@gmail.com>
2018-01-25 20:27:52 +0800

    InflaterInputStream example

---
+++ Java IO/InflaterInputStreamDemo.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.InflaterInputStream;
+
+/**
+ * Created by Darush on 1/25/2018.
+ */
+public class InflaterInputStreamDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream input = new FileInputStream("files/DeflaterOutputStreamDemo.txt");
+            FileOutputStream output = new FileOutputStream("files/DeflaterOutputStreamDemo.java");
+            InflaterInputStream inflater = new InflaterInputStream(input);
+
+            int i;
+            while ((i = inflater.read()) != -1) {
+                output.write((byte) i);
+            }
+
+            inflater.close();
+            output.close();
+            System.out.println("Successfully decompressed the file...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
