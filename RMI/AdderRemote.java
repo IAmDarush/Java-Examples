11f1cf7826d86e1ea92ec80e5c3e3181dc4f5c6c
DarushDev <darushfaqiri@gmail.com>
2018-02-01 00:32:31 +0800

    Provide the implementation of the remote interface

---
+++ RMI/AdderRemote.java
@@ -0,0 +1,14 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class AdderRemote extends UnicastRemoteObject implements Adder {
+
+    protected AdderRemote() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public int add(int x, int y) throws RemoteException {
+        return x + y;
+    }
+}
