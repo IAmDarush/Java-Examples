36397323da5e9553743340406d560b0d8408d076
DarushDev <darushfaqiri@gmail.com>
2018-02-01 00:54:23 +0800

    create Customer and Bank models

---
+++ RMI/example2/Bank.java
@@ -0,0 +1,9 @@
+package example2;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface Bank extends Remote {
+    List<Customer> getCustomers() throws RemoteException;
+}
