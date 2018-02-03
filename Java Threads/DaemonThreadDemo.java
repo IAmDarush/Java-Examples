442184475ae46e03313608b9742736e9181c1540
DarushDev <darushfaqiri@gmail.com>
2018-02-03 15:32:08 +0800

    Daemon thread example

---
+++ Java Threads/DaemonThreadDemo.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Darush on 2/3/2018.
+ */
+public class DaemonThreadDemo extends Thread {
+
+    @Override
+    public void run() {
+        if(Thread.currentThread().isDaemon()) {
+            System.out.println("Daemon thread is working " + Thread.currentThread().getName());
+        } else {
+            System.out.println("User thread is working" + Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DaemonThreadDemo t1 = new DaemonThreadDemo();
+        DaemonThreadDemo t2 = new DaemonThreadDemo();
+        DaemonThreadDemo t3 = new DaemonThreadDemo();
+
+        t1.setDaemon(true);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
