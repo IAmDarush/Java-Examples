2dd3eb30380e513634d5ecd911f2fad9bc3328c7
DarushDev <darushfaqiri@gmail.com>
2018-02-06 23:27:23 +0800

    try-catch example

---
+++ Java Exceptions/TryCatchDemo.java
@@ -0,0 +1,15 @@
+public class TryCatchDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int result = 30/0;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Rest of the code...");
+
+    }
+
+}
