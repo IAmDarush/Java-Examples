497fe52a2b5ff6fedd8eaa4bc733d913f5930da7
DarushDev <darushfaqiri@gmail.com>
2018-02-09 20:15:42 +0530

    throw keyword example

---
+++ Java Exceptions/ThrowDemo.java
@@ -0,0 +1,18 @@
+public class ThrowDemo {
+
+    public static void main(String[] args) {
+
+        validate(13);
+        System.out.println("Welcome to vote");
+
+    }
+
+    static void validate(int age) {
+        if(age <18) {
+            throw new ArithmeticException("Not valid integer");
+        } else {
+            System.out.println("You are eligible to vote");
+        }
+    }
+
+}
