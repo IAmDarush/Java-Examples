b65c8b13b90783a2b580eb70cc6a04207f712e7f
DarushDev <darushfaqiri@gmail.com>
2018-02-15 14:49:42 +0530

    Anonymous inner class example

---
+++ Java Inner Class/AnonymousInnerClassDemo.java
@@ -0,0 +1,22 @@
+abstract class Person {
+    abstract void eat();
+}
+
+public class AnonymousInnerClassDemo {
+
+    public static void main(String[] args) {
+
+        Person person = new Person() {
+            @Override
+            void eat() {
+                System.out.println("I am eating...");
+            }
+        };
+
+        person.eat();
+
+    }
+
+}
+
+
