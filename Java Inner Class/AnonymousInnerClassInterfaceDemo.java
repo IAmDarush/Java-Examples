8ad8cddbe56ca8cc386f7508173a1d2cfb750f28
DarushDev <darushfaqiri@gmail.com>
2018-02-15 15:04:11 +0530

    Anonymous inner class using interface example

---
+++ Java Inner Class/AnonymousInnerClassInterfaceDemo.java
@@ -0,0 +1,19 @@
+interface Eatable {
+    void eat();
+}
+
+public class AnonymousInnerClassInterfaceDemo {
+
+    public static void main(String[] args) {
+
+        Eatable eatable = new Eatable() {
+            public void eat() {
+                System.out.println("I am eating...");
+            }
+        };
+
+        eatable.eat();
+
+    }
+
+}
