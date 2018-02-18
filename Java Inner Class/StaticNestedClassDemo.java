74aa00cb1fe15623f7011e1590363e14d52388de
DarushDev <darushfaqiri@gmail.com>
2018-02-18 12:00:29 +0530

    Adding static method to static nested class

--- Java Inner Class/StaticNestedClassDemo.java
+++ Java Inner Class/StaticNestedClassDemo.java
@@ -8,12 +8,20 @@ public class StaticNestedClassDemo {
             System.out.println("Data = " + data);
         }
 
+        static void staticMethod() {
+            System.out.println("Data = " + data);
+        }
+
     }
 
     public static void main(String[] args) {
 
+        // calling non-static method with object instance
         StaticNestedClassDemo.Inner inner = new StaticNestedClassDemo.Inner();
         inner.msg();
+
+        // Calling the static method without any object instance
+        StaticNestedClassDemo.Inner.staticMethod();
 
     }
 
