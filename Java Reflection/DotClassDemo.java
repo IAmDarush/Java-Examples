bb684bb52b6ad77660bf2e6645f98248a0b88f63
DarushDev <darushfaqiri@gmail.com>
2018-02-19 22:29:01 +0530

    .class example

---
+++ Java Reflection/DotClassDemo.java
@@ -0,0 +1,11 @@
+public class DotClassDemo {
+    public static void main(String[] args) {
+
+        Class c = boolean.class;
+        System.out.println(c.getName());
+
+        Class c2 = DotClassDemo.class;
+        System.out.println(c2.getName());
+
+    }
+}
