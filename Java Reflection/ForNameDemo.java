8f3775cc6889342f90294ba048b6a4566c73eab2
DarushDev <darushfaqiri@gmail.com>
2018-02-19 22:06:12 +0530

    Class.forName() example

---
+++ Java Reflection/ForNameDemo.java
@@ -0,0 +1,19 @@
+class Sample {
+}
+
+public class ForNameDemo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Class sample = Class.forName("Sample");
+            System.out.println(sample.getName());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
