b7a5fd840867719f5e745e5b31684f1a99117695
DarushDev <darushfaqiri@gmail.com>
2018-02-22 22:25:50 +0530

    Creating your own applet viewer example

---
+++ Java Reflection/MyApplet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class MyApplet extends Applet {
+
+    @Override
+    public void paint(Graphics g) {
+
+        g.drawString("Welcome", 50, 50 );
+
+    }
+
+}
