5452a3f5193d37f3cfb9c4c1ba5861f6e9649abc
DarushDev <darushfaqiri@gmail.com>
2018-02-26 22:27:44 +0530

    Using ZoneId class to get the time in a time zone

--- Java Date Time/LocalTimeDemo.java
+++ Java Date Time/LocalTimeDemo.java
@@ -1,5 +1,6 @@
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class LocalTimeDemo {
 
@@ -18,6 +19,24 @@ public class LocalTimeDemo {
         System.out.println(time3 + " : Subtract 1 hour and 5 minutes");
         LocalTime time4 = time3.plusHours(1).plusMinutes(5);
         System.out.println(time4 + " : Add 1 hour and 5 minutes");
+
+        // Get the time zone
+        ZoneId kabulZone = ZoneId.of("Asia/Kabul");
+        ZoneId kolkataZone = ZoneId.of("Asia/Kolkata");
+
+        // Get current time in these time zones
+        LocalTime kabulTime = LocalTime.now(kabulZone);
+        LocalTime kolkataTime = LocalTime.now(kolkataZone);
+
+        System.out.println("Afghanistan Time : " + kabulTime);
+        System.out.println("India Time: " + kolkataTime);
+
+        // Find the difference of time between given time zones
+        long hoursDiff = ChronoUnit.HOURS.between(kabulTime, kolkataTime);
+        long minutesDiff = ChronoUnit.MINUTES.between(kabulTime, kolkataTime);
+
+        System.out.println("Hours between two time zones: " + hoursDiff);
+        System.out.println("Minutes between two time zones: " + minutesDiff);
 
     }
 
