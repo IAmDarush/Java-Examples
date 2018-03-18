f423385dbb4cd9a138082ca884008872dc48b6d9
DarushDev <darushfaqiri@gmail.com>
2018-03-18 21:55:58 +0530

    TimeZone example

---
+++ Java Date Time/TimeZoneDemo.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class TimeZoneDemo {
+
+    public static void main(String[] args) {
+
+        // Get the list of all available IDs in TimeZone class
+        String[]  ids = TimeZone.getAvailableIDs();
+        System.out.println("Available IDS: ");
+        for (String id : ids) {
+            System.out.println(id);
+        }
+
+        // Get the amount of offset in milliseconds
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Kabul");
+        System.out.println("Offset value of TimeZone: "
+                + timeZone.getOffset(Calendar.ZONE_OFFSET));
+
+        System.out.println("Time zone ID is : " + timeZone.getID());
+        System.out.println("Time zone display name is : " + timeZone.getDisplayName());
+
+    }
+
+}
