7280a344390db760c2d29fa12167a1c68053e9c3
DarushDev <darushfaqiri@gmail.com>
2018-03-29 20:59:10 +0530

    Internationalizing number example

---
+++ Internationalization/InternationalizingNumberDemo.java
@@ -0,0 +1,22 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class InternationalizingNumberDemo {
+
+    public static void main(String[] args) {
+
+        printNumber(Locale.US);
+        printNumber(Locale.CHINA);
+        printNumber(Locale.JAPAN);
+
+    }
+
+    static void printNumber(Locale locale) {
+
+        NumberFormat formatter = NumberFormat.getNumberInstance(locale);
+        String number = formatter.format(123000.0001);
+        System.out.println(number + " for the locale " + locale);
+
+    }
+
+}
