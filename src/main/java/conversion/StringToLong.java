2483f3b308e00bf940344522f7de0cd77e714457
DarushDev <darushfaqiri@gmail.com>
2018-04-05 22:27:22 +0530

    String to Long example

---
+++ src/main/java/conversion/StringToLong.java
@@ -0,0 +1,13 @@
+package main.java.conversion;
+
+public class StringToLong {
+
+    public static void main(String[] args) {
+
+        String s = "12345678910";
+        Long l = Long.parseLong(s);
+        System.out.println(l);
+
+    }
+
+}
