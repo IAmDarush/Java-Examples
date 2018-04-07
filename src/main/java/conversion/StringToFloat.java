fa12cf0715081e1c1c01b14a821b1dbaea4e1012
DarushDev <darushfaqiri@gmail.com>
2018-04-07 21:01:17 +0530

    String to Float example

---
+++ src/main/java/conversion/StringToFloat.java
@@ -0,0 +1,13 @@
+package main.java.conversion;
+
+public class StringToFloat {
+
+    public static void main(String[] args) {
+
+        String s = "123.90";
+        Float f = Float.parseFloat(s);
+        System.out.println(10 + f);
+
+    }
+
+}
