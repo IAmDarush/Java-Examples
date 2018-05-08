0970243c5427f1259ab9cfcbc26cb5862979969c
DarushDev <darushfaqiri@gmail.com>
2018-05-08 23:45:44 +0430

    Timestamp to Date

---
+++ src/main/java/conversion/TimestampToDate.java
@@ -0,0 +1,16 @@
+package main.java.conversion;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class TimestampToDate {
+
+    public static void main(String[] args) {
+
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        Date date = new Date(ts.getTime());
+        System.out.println(date);
+
+    }
+
+}
