88a63ae859a07638efd316c7cfc7284998849a8d
DarushDev <darushfaqiri@gmail.com>
2018-05-11 18:56:44 +0430

    Decimal to Binary example

---
+++ src/main/java/conversion/DecimalToBinary.java
@@ -0,0 +1,11 @@
+package main.java.conversion;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.toBinaryString(20));
+
+    }
+
+}
