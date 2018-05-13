03a8e055be356301b2b51d42597d1d405adcf4a7
DarushDev <darushfaqiri@gmail.com>
2018-05-14 02:17:49 +0430

    Hexadecimal to Decimal

---
+++ src/main/java/conversion/DecimalToHexadecimal.java
@@ -0,0 +1,12 @@
+package main.java.conversion;
+
+public class DecimalToHexadecimal {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.toHexString(15));
+        System.out.println(Integer.toHexString(100));
+
+    }
+
+}
