1863510adba6b730edaddf0d6429b563335ab520
DarushDev <darushfaqiri@gmail.com>
2018-05-20 01:41:08 +0430

    Decimal to Octal example

---
+++ src/main/java/conversion/DecimalToOctal.java
@@ -0,0 +1,11 @@
+package main.java.conversion;
+
+public class DecimalToOctal {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.toOctalString(8));
+
+    }
+
+}
