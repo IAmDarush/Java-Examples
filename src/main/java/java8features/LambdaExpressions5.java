e5150ef832d2478d670f1b6befd2513cdd0e5e06
DarushDev <darushfaqiri@gmail.com>
2018-05-30 22:59:50 +0430

    Lambda Expressions using Comparator

---
+++ src/main/java/java8features/LambdaExpressions5.java
@@ -0,0 +1,43 @@
+package main.java.java8features;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Product {
+
+    int id;
+    String name;
+    float price;
+
+    public Product(int id, String name, float price) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+public class LambdaExpressions5 {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product(1, "Apple Laptop", 30000f));
+        list.add(new Product(2, "Mouse", 12000f));
+        list.add(new Product(3, "Keyboard", 15000f));
+
+        System.out.println("Sort by name:");
+
+        Collections.sort(list, (p1, p2) -> {
+            return p1.name.compareTo(p2.name);
+        });
+
+        for (Product p : list) {
+            System.out.println(p.id + " " + p.name + " " + p.price);
+        }
+
+    }
+
+}
