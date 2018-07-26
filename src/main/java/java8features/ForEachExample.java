89e549d83fb77f88d684d04c249d2c02e2a7b43f
DarushDev <darushfaqiri@gmail.com>
2018-07-26 23:10:00 +0800

    forEach() method example

---
+++ src/main/java/java8features/ForEachExample.java
@@ -0,0 +1,24 @@
+package main.java.java8features;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Darush on 7/26/2018.
+ */
+public class ForEachExample {
+
+    public static void main(String[] args) {
+
+        List<String> fruitList = new ArrayList<>();
+        fruitList.add("Apple");
+        fruitList.add("Orange");
+        fruitList.add("Water Melon");
+        fruitList.add("Pineapple");
+
+        System.out.println("Iterating using lambda expression:------");
+        fruitList.forEach(fruit -> System.out.println(fruit));
+
+    }
+
+}
